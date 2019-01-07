fb1f89d0a6942bc2dc230213761cb1647ec77264
Johnnyboyathin <john.n739@slcstudents.org>
2019-01-07 09:48:29 -0700

    changes
    There was a mistake in the wording

--- myScript.js
+++ myScript.js
@@ -1,3 +1,19 @@
+//This function changed the text in the demo paragraph
+
 function myFunction() {
-    document.getElemntById('demo').innerHTML = "Paragraph Changed";
+    document.getElementById('demo').innerHTML = "Paragraph Changed";
+}
+
+//defining varibles
+
+var x, y, z;
+x = 5;
+y = 6;
+z = x + y;
+
+//This function changes the text in the math paragraph
+
+function mathFunction () {
+    document.getElementById('math').innerHTML =z;
+    
 }
